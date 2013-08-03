e54a40df6febdc9f146ba46361c1c64e37b1b690
Andrew O'Connor <aoconnr@gmail.com>
2013-08-03 16:30:27 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VKB::Application.config.secret_token = 'dbc0b9552c873a12fe3b39e8e9b976927fea9b8283d6291bb886a5271059e6b7058c9ad4ad680b4111b5f92d4d8c29f6daa0b52ec55563f698d5c6162ed5d932'
