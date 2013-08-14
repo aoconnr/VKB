b01a5f70afc2047bfee64efd9b20f4060b74e71d
Andrew O'Connor <aoconnr@gmail.com>
2013-08-14 16:19:30 +0100

    added uploads folder to gitignore

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,18 @@ class Item < ActiveRecord::Base
   attr_accessible :image, :name, :price
   mount_uploader :image, ImageUploader
 
-  before_destroy :remember_image
+  before_destroy :remember_id
   after_destroy :remove_image_directory 
 
   monetize :price, :as => "cost"
 
   has_one :type
 
-  def remember_image
-    @image = image
+  def remember_id
+    @id = id
   end
 
   def remove_image_directory
-    FileUtils.remove_dir("#{Rails.root}/public/images/#{@image}", :force => true)
+    FileUtils.remove_dir("#{Rails.root}/uploads/#{@id}", :force => true)
   end
 end
